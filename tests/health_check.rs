43d7beea3e28218434d871d4e5c06cf79cf15171
ryuma017 <tgcrym01@gmail.com>
2022-05-12 19:39:04 +0900

    capture requirements as tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 use newsletter::run;
+use reqwest::Client;
 use std::net::TcpListener;
 
 fn spawn_app() -> String {
@@ -25,4 +26,54 @@ async fn haelth_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=ryuma%20taguchi&email=ryuma017%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=ryuma%20taguchi", "missing the email"),
+        ("email=ryuma017%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencorded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
 }
