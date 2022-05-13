a39ab65b64cec3778118f42d1c5cfd8fc9ba7eec
ryuma017 <tgcrym01@gmail.com>
2022-05-13 23:28:55 +0900

    modify the `run` method to accept a `PgConnection` alongside the `TcpListener`

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
