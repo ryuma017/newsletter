960f9bbb984d79adeac6661dc48c41d85d27b617
ryuma017 <tgcrym01@gmail.com>
2022-05-16 00:25:36 +0900

    improve the observability posture

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use std::net::TcpListener;
 
-use env_logger::Env;
 use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
 
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // 全ての `log` の event を subscriber にリダイレクトする
+    LogTracer::init().expect("Failed to set logger.");
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("newsletter".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
