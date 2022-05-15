dd52401c7bf182f6d7d84a2a75bdc7cacb970054
ryuma017 <tgcrym01@gmail.com>
2022-05-15 21:36:19 +0900

    instrumenting subscribe handler

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -11,16 +12,19 @@ pub struct FormData {
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+    // `info_span!` は info-level の span を作る
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
     );
-    tracing::info!(
-        "request_id {} - Saving new subscriber details in the database.",
-        request_id
-    );
+    let _request_span_guard = request_span.enter();
+    // `_request_span_guard` は `subscribe` の最後に drop され、span を抜ける
+
+    // `query_span` に対して `.enter()` は呼ばない
+    // `.instrument()` がクエリの future のライフタイムにおいて、適切なタイミングで処理してくれる
+    let query_span = tracing::info_span!("Saving new subscriber details in thd database.");
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -32,7 +36,8 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now()
     )
     .execute(pool.get_ref())
-    .await
+    .instrument(query_span) // まず instrumentation をアタッチし、
+    .await                  // `.await` する
     {
         Ok(_) => {
             tracing::info!(
@@ -42,11 +47,8 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            // この error log は `query_span` の外
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
