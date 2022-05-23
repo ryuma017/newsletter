be7784bbbaebdf06b5b8e70c48f317ce02bd3309
ryuma017 <tgcrym01@gmail.com>
2022-05-23 22:25:01 +0900

    adapt the logic of the `POST` `/subscriptions` request handler to match the new requirements (#20)

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -20,14 +20,6 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
-
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
-        .fetch_one(&app.db_pool)
-        .await
-        .expect("Failed to fetch saved subscription.");
-
-    assert_eq!(saved.email, "ryuma017@gmail.com");
-    assert_eq!(saved.name, "ryuma taguchi")
 }
 
 #[tokio::test]
@@ -96,4 +88,30 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
 
     // Assert
     // Mock asserts on drop
+}
+
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=ryuma%20taguchi&email=ryuma017%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_subscriptions(body.into()).await;
+
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ryuma017@gmail.com");
+    assert_eq!(saved.name, "ryuma taguchi");
+    assert_eq!(saved.status, "pending_confirmation");
 }
