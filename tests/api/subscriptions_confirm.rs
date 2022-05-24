aea957dad65449e1a323bd0c4d44f2311458efe7
ryuma017 <tgcrym01@gmail.com>
2022-05-24 10:35:17 +0900

    add red test to check the `status` of the subscriber stored in the database (#20)

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,4 +1,3 @@
-use reqwest::Url;
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, ResponseTemplate};
 
@@ -39,4 +38,38 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=ryuma%20taguchi&email=ryuma017%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(email_request);
+
+    // Act
+    reqwest::get(confirmation_links.html)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ryuma017@gmail.com");
+    assert_eq!(saved.name, "ryuma taguchi");
+    assert_eq!(saved.status, "confirmede");
 }
