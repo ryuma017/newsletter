fc32cc402803b1c0d970a9f15d6248ab99804ab7
ryuma017 <tgcrym01@gmail.com>
2022-06-05 15:02:46 +0900

    remove the timing difference between an auth failure due to an invalid password and an auth failure due to a non-existent username (#35)

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -166,20 +166,32 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let (user_id, expected_password_hash) = get_stored_credentials(&credentials.username, pool)
-        .await
-        .map_err(PublishError::UnexpectedError)?
-        .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
-    // let current_span = tracing::Span::current();
-    // tokio::task::spawn_blocking(move || current_span.in_scope(|| verify_password_hash(/* */)));
-    // ↑ 同義 ↓
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+        gZiV/M1gPc22ElAH/Jh1Hw$\
+        CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
+
+    // store された credentials が見つかった場合のみ `Some` で wrap して set される
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_credentials(&credentials.username, pool)
+            .await
+            .map_err(PublishError::UnexpectedError)?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
+
     spawn_blocking_with_tracing(move || {
         verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
     .context("Failed to spawn blocking task.")
     .map_err(PublishError::UnexpectedError)??;
-    Ok(user_id)
+
+    user_id.ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))
 }
 
 #[tracing::instrument(
