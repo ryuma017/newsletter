ddba2611835edb38375dd4fde949956406d38531
ryuma017 <tgcrym01@gmail.com>
2022-06-06 14:44:19 +0900

    add a dummy home page endpoint, `GET` `/` (#37)

--- src/startup.rs
+++ src/startup.rs
@@ -9,7 +9,7 @@ use tracing_actix_web::TracingLogger;
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
+use crate::routes::{confirm, health_check, home, publish_newsletter, subscribe};
 
 pub struct Application {
     port: u16,
@@ -79,6 +79,7 @@ pub fn run(
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .route("/", web::get().to(home))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
