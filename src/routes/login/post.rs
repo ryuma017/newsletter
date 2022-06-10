e4647148ff5d1cb0c34fc56b471316adbdaabcfa
ryuma017 <tgcrym01@gmail.com>
2022-06-10 21:59:29 +0900

    improve `/login` request handler (#37)

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,4 +1,4 @@
-use actix_web::http::header::LOCATION;
+use actix_web::http::header::{ContentType, LOCATION};
 use actix_web::http::StatusCode;
 use actix_web::{web, HttpResponse, ResponseError};
 use secrecy::Secret;
@@ -53,10 +53,14 @@ impl std::fmt::Debug for LoginError {
 }
 
 impl ResponseError for LoginError {
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        let encoded_error = urlencoding::Encoded::new(self.to_string());
+        HttpResponse::build(self.status_code())
+            .insert_header((LOCATION, format!("/login?error={}", encoded_error)))
+            .finish()
+    }
+
     fn status_code(&self) -> StatusCode {
-        match self {
-            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
-        }
+        StatusCode::SEE_OTHER
     }
 }
