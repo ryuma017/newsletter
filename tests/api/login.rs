f401598fa969b3023eac865dc892430dc04d43d0
ryuma017 <tgcrym01@gmail.com>
2022-06-11 12:17:58 +0900

    change `TestApp::spawn_app` to create and store an instance of `reqwest::Client` which we will in turn use in all its helper methods (#37)

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,4 +1,4 @@
-use crate::helpers::{assert_is_redirect_to, spawn_app};
+use crate::helpers::spawn_app;
 
 #[tokio::test]
 async fn an_error_flash_message_is_set_on_failure() {
@@ -15,5 +15,8 @@ async fn an_error_flash_message_is_set_on_failure() {
     // Assert
     let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
     assert_eq!(flash_cookie.value(), "Authentication failed");
-    // assert_is_redirect_to(&response, "/login");
+
+    // Act - 2
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
 }
