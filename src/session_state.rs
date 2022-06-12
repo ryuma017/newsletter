12aee02d7ab0c5da5f011d02c3707d81d8e6209f
ryuma017 <tgcrym01@gmail.com>
2022-06-12 23:45:48 +0900

    define a typed interface to session, `TypedSession` (#39)

---
+++ src/session_state.rs
@@ -0,0 +1,33 @@
+use std::future::{ready, Ready};
+
+use actix_session::{Session, SessionExt};
+use actix_web::dev::Payload;
+use actix_web::{FromRequest, HttpRequest};
+use uuid::Uuid;
+
+pub struct TypedSession(Session);
+
+impl TypedSession {
+    const USER_ID_KEY: &'static str = "user_id";
+
+    pub fn renew(&self) {
+        self.0.renew();
+    }
+
+    pub fn insert_user_id(&self, user_id: Uuid) -> Result<(), serde_json::Error> {
+        self.0.insert(Self::USER_ID_KEY, user_id)
+    }
+
+    pub fn get_user_id(&self) -> Result<Option<Uuid>, serde_json::Error> {
+        self.0.get::<Uuid>(Self::USER_ID_KEY)
+    }
+}
+
+impl FromRequest for TypedSession {
+    type Error = <Session as FromRequest>::Error;
+    type Future = Ready<Result<Self, Self::Error>>;
+
+    fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
+        ready(Ok(TypedSession(req.get_session())))
+    }
+}
