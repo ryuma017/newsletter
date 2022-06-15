cdbe111f864b90a8a3193ef9063d1af2cb31025f
ryuma017 <tgcrym01@gmail.com>
2022-06-15 23:51:37 +0900

    factor login to get user_id out as a new `reject_anonymous_users` function (#46)

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -2,6 +2,8 @@ use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
+use actix_web::error::InternalError;
+use uuid::Uuid;
 
 use crate::authentication::{validate_credentials, AuthError, Credentials};
 use crate::routes::admin::dashboard::get_username;
@@ -20,12 +22,7 @@ pub async fn change_password(
     session: TypedSession,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let user_id = session.get_user_id().map_err(e500)?;
-    // Check if the user is logged in
-    if user_id.is_none() {
-        return Ok(see_other("/login"));
-    };
-    let user_id = user_id.unwrap();
+    let user_id = reject_anonymous_users(session).await?;
     // Check if the new two passwords given from the form match
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
@@ -53,4 +50,15 @@ pub async fn change_password(
         .map_err(e500)?;
     FlashMessage::info("Your password has been changed.").send();
     Ok(see_other("/admin/password"))
+}
+
+async fn reject_anonymous_users(session: TypedSession) -> Result<Uuid, actix_web::Error> {
+    match session.get_user_id().map_err(e500)? {
+        Some(user_id) => Ok(user_id),
+        None => {
+            let response = see_other("/login");
+            let e = anyhow::anyhow!("The user has not logged in.");
+            Err(InternalError::from_response(e, response).into())
+        }
+    }
 }
