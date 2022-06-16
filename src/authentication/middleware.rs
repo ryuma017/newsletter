2e6c62c5899e2cc801d9ba08c85f08cc71218ba8
ryuma017 <tgcrym01@gmail.com>
2022-06-16 22:12:29 +0900

    copy over everything from `authentication.rs` to `authentication/password.rs` & create middleware skeleton (#46)

---
+++ src/authentication/middleware.rs
@@ -0,0 +1,7 @@
+use actix_web_lab::middleware::Next;
+use actix_web::body::MessageBody;
+use actix_web::dev::{ServiceResponse, ServiceRequest};
+
+pub async fn reject_anonymous_users() {
+    unimplemented!()
+}
---
+++ src/authentication/mod.rs
@@ -0,0 +1,5 @@
+mod password;
+mod middleware;
+
+pub use password::{change_password, validate_credentials, AuthError, Credentials};
+pub use middleware::reject_anonymous_users;
