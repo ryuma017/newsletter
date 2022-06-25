7e81ff1546b1475285f2be4c565cb101d4242454
ryuma017 <tgcrym01@gmail.com>
2022-06-25 14:30:26 +0900

    plug `save_response` into the request handler (#56)

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -1,6 +1,7 @@
 use actix_web::body::to_bytes;
 use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
+use sqlx::postgres::PgHasArrayType;
 use sqlx::PgPool;
 use uuid::Uuid;
 
@@ -11,6 +12,12 @@ use super::IdempotencyKey;
 struct HeaderPairRecord {
     name: String,
     value: Vec<u8>,
+}
+
+impl PgHasArrayType for HeaderPairRecord {
+    fn array_type_info() -> sqlx::postgres::PgTypeInfo {
+        sqlx::postgres::PgTypeInfo::with_name("_header_pair")
+    }
 }
 
 pub async fn get_saved_response(
@@ -65,7 +72,26 @@ pub async fn save_response(
         h
     };
 
-    // TODO: SQL query
+    sqlx::query_unchecked!(
+        r#"
+        INSERT INTO idempotency (
+            user_id,
+            idempotency_key,
+            response_status_code,
+            response_headers,
+            response_body,
+            created_at
+        )
+        VALUES ($1, $2, $3, $4, $5, now())
+        "#,
+        user_id,
+        idempotency_key.as_ref(),
+        status_code,
+        headers,
+        body.as_ref()
+    )
+    .execute(pool)
+    .await?;
 
     let http_response = response_head.set_body(body).map_into_boxed_body();
     Ok(http_response)
--- src/routes/admin/newsletters/post.rs
+++ src/routes/admin/newsletters/post.rs
@@ -7,7 +7,7 @@ use sqlx::PgPool;
 use crate::authentication::UserId;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
-use crate::idempotency::{get_saved_response, IdempotencyKey};
+use crate::idempotency::{get_saved_response, save_response, IdempotencyKey};
 use crate::utils::{e400, e500, see_other};
 
 #[derive(serde::Deserialize)]
@@ -66,7 +66,11 @@ pub async fn publish_newsletter(
         }
     }
     FlashMessage::info("The newsletter issue has been published.").send();
-    Ok(see_other("/admin/newsletters"))
+    let response = see_other("/admin/newsletters");
+    let response = save_response(&pool, &idempotency_key, *user_id, response)
+        .await
+        .map_err(e500)?;
+    Ok(response)
 }
 
 struct ConfirmedSubscriber {
